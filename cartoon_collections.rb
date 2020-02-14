ee123e4e1eb727586f86a64ac6345825b25878c4
Daniel Urbina <danielur1999@gmail.com>
2020-02-14 07:46:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,8 @@ def long_planeteer_calls(long_planeteer_calls)
   answer 
 end
 
-def find_the_cheese()
+def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
