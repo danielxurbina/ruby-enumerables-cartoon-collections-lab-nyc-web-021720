49f631be6384339c776bf65f29a60fc6c8130e52
Daniel Urbina <danielur1999@gmail.com>
2020-02-14 07:43:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(dwarfs)
 end
 
 def summon_captain_planet(veggies)
-  veggies.collect do |
+  veggies.collect do |call|
+    
 end
 
 def long_planeteer_calls# code an argument here
