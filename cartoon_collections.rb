77e11e5b7a7b6110e7c14ea5e73ada63dd37c4ad
Daniel Urbina <danielur1999@gmail.com>
2020-02-14 07:44:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,11 +10,11 @@ def summon_captain_planet(veggies)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls()
+  
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese()
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
